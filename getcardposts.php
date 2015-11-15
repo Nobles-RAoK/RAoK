4e83b0f6a1153d2fd21f7ab21cd3ec4a1e548056
thatdamnkorean <iriley18@nobles.edu>
2015-11-14 20:24:31 -0500

    thus it begins
    can you change newport.html to .php plz? :D
    also I set up a connection to the database, allowing for the adding of
    posts, the checking of whether a post already exists, and a typeable
    spot to enter the post

---
+++ getcardposts.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+  
+<body>
+    <?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'RAoK');
+
+if (!$link) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Success... ' . mysqli_get_host_info($link) . "\n";
+
+mysqli_close($link);
+    
+    ?>
+    </body>
+</html>
